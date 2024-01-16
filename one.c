36076050e73cec3f8ba953eb57d26705b784c235
Andrew Praskala <84627619+aprask@users.noreply.github.com>
2024-01-16 08:30:54 -0500

    Update one.c

--- one.c
+++ one.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!");
+    printf("Hello, World!\n");
+    char ch = 'A';
+    printf("ch value is %d which is the ASCII value of %c\n", ch, ch); // we can acquire the ascii decimal value via %d
+    // Scanners
+    int number;
+    printf("What is your age?\n");
+    scanf("%d",&number); // pointer?
+    printf("%d",number);
     return 0;
 }
